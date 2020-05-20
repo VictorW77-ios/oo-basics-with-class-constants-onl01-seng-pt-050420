6eb5af4b2bdb8ef5e1eefc4c602c18961c2ece3c
Victor Williams <victor.williams1030@gmail.com>
2020-05-20 13:31:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,13 +7,13 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-    BRANDS << brandBRANDS.uniq(&:size)
+    BRANDS << brand
+    BRANDS.uniq(&:size)
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  
   
 end
